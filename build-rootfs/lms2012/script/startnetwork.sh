4c82442b5505cc7b3ebdfb95b9196ffdc75a8881
topikachu <topikachu@163.com>
2013-11-20 10:48:56 +0800

    try to use debain network configuration instead of script. no test yet!

--- build-rootfs/lms2012/script/startnetwork.sh
+++ build-rootfs/lms2012/script/startnetwork.sh
@@ -3,14 +3,14 @@ NETADDRESS=10.0.1.1
 
 
 # create the bridge and configure it
-brctl addbr br0
-ifconfig br0 ${NETADDRESS}
-brctl setfd br0 0
-brctl stp br0 off
+#brctl addbr br0
+#ifconfig br0 ${NETADDRESS}
+#brctl setfd br0 0
+#brctl stp br0 off
 # setup the USB interface
 modprobe g_ether
-ifconfig usb0 0.0.0.0
-brctl addif br0 usb0
+#ifconfig usb0 0.0.0.0
+#brctl addif br0 usb0
 # start dhcpd
 
 # configure Bluetooth
