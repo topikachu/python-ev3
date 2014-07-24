faca4cbc8050a2c6c9521e34d0e8b3e77b3e2271
Gong Yi <topikachu@163.com>
2014-07-24 10:19:32 +0200

    script to create deb

--- smbus-build/build.sh
+++ smbus-build/build.sh
@@ -3,18 +3,23 @@ then
 echo "Sorry, you are not root!"
 exit 1
 fi
+export DEBIAN_FRONTEND=noninteractive
+export DEBCONF_NONINTERACTIVE_SEEN=true
+export LC_ALL=C LANGUAGE=C LANG=C
+
 apt-get install qemu-user-static multistrap
 export TARGET_ROOTFS_DIR=ev3-rootfs
 multistrap -f multistrap.conf -d ${TARGET_ROOTFS_DIR}
 cp /usr/bin/qemu-arm-static ${TARGET_ROOTFS_DIR}/usr/bin
+cp /etc/resolv.conf ev3-rootfs/etc/
 mkdir -p ${TARGET_ROOTFS_DIR}/dev
 mount --bind /dev ${TARGET_ROOTFS_DIR}/dev
-chroot ${TARGET_ROOTFS_DIR} var/lib/dpkg/info/dash.preinst install
+chroot ${TARGET_ROOTFS_DIR} var/lib/dpkg/info/dash.preinst install > /dev/null
 chroot ${TARGET_ROOTFS_DIR} dpkg --configure -a
 cp -r py-smbus-python3 ${TARGET_ROOTFS_DIR}/home/py-smbus-python3
 chroot ${TARGET_ROOTFS_DIR} /home/py-smbus-python3/build-smbus.sh
 mkdir -p py-smbus-python3/build
-cp -r ${TARGET_ROOTFS_DIR}/home/py-smbus-python3/build/lib.linux-armv* py-smbus-python3/build
+cp -r ${TARGET_ROOTFS_DIR}/home/py-smbus-python3/*.deb py-smbus-python3/build
 umount ${TARGET_ROOTFS_DIR}/dev
 ls ${TARGET_ROOTFS_DIR}/dev
 echo "''''''''''''''''''''''''''"
--- smbus-build/py-smbus-python3/build-smbus.sh
+++ smbus-build/py-smbus-python3/build-smbus.sh
@@ -1,4 +1,10 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH
-python3 setup.py build
+export LANG=C
+rm -r i2c-tools-3.1.1
+rm *.deb
+apt-get source i2c-tools
+cd i2c-tools-3.1.1
+patch -p1<../python3.patch
+dpkg-buildpackage -uc -B
