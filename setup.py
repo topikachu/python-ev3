0f02defc7883eeeaeb3a6b5aec20a26153db47ac
pikachu <topikachu@163.com>
2014-08-17 13:38:10 +0800

    Prepare 0.0.2.2 release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools  import setup
 
 setup(name='python-ev3',
-      version='0.0.2.1',
+      version='0.0.2.2',
       license='Apache License 2.0',
       description='Python library of ev3dev firmware',
       author='Gong Yi',
       author_email='topikachu@163.com',
       url='https://github.com/topikachu/python-ev3',
       packages=['ev3'],
-      download_url="https://github.com/topikachu/python-ev3/releases/tag/0.0.2.1"
+      download_url="https://github.com/topikachu/python-ev3/releases/tag/0.0.2.2"
      )
