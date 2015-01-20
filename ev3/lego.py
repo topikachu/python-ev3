a75084ce871c5c51e3c57ef805c18c2040a8a97d
Jesper Friis <jesper.friis@sintef.no>
2015-01-20 20:30:52 +0100

    Added some definitions for the remote control and some docstrings

--- ev3/lego.py
+++ ev3/lego.py
@@ -46,6 +46,7 @@ class SoundSensor(LegoSensor):
         # Sound pressure level (0 to 1000)
         return "%.2f" % (self.value0/(int(self.decimals)*10))
 
+
 class ColorSensor(LegoSensor):
     colors = (None, 'black', 'blue', 'green',
               'yellow', 'red', 'white', 'brown')
@@ -55,26 +56,38 @@ class ColorSensor(LegoSensor):
 
     @property
     def rgb(self):
+        """Raw color components; (`r`, `g`, `b`). Values between 0 and 1020(??).
+
+        All leds rapidly cycle, appears white."""
         self.mode = 'RGB-RAW'
         return self.value0, self.value1, self.value2
 
     @property
     def color(self):
+        """Integer between 0 and 7 indicating the color. See the `colors`
+        attribute for color names.
+
+        All leds rapidly cycle, appears white."""
         self.mode = 'COL-COLOR'
         return self.value0
 
     @property
     def reflect(self):
+        """Reflected intensity in percent (int). Red led is on."""
         self.mode = 'COL-REFLECT'
         return self.value0
 
     @property
     def ambient(self):
+        """Ambient intensity in percent (int). Red led is off."""
         self.mode = 'COL-AMBIENT'
         return self.value0
 
     @property
     def ref_raw(self):
+        """Raw reflected intensity; (`r`, `b`). Values between 0 and 1020(??).
+
+        Red led is on."""
         self.mode = 'REF-RAW'
         return self.value0, self.value1
 
@@ -84,8 +97,26 @@ class InfraredSensor(LegoSensor):
     def __init__(self, port=-1):
        LegoSensor.__init__(self, port, name='ev3-uart-33')
 
+    class REMOTE:
+        """Button values for the `remote` property."""
+        NONE = 0
+        RED_UP = 1
+        RED_DOWN = 2
+        BLUE_UP = 3
+        BLUE_DOWN = 4
+        RED_UP_AND_BLUE_UP = 5
+        RED_UP_AND_BLUE_DOWN = 6
+        RED_DOWN_AND_BLUE_UP = 7
+        RED_DOWN_AND_BLUE_DOWN = 8
+        BAECON_MODE_ON = 9
+        RED_UP_AND_RED_DOWN = 10
+        BLUE_UP_AND_BLUE_DOWN = 11
+
     @property
     def remote(self):
+        """IR remote control mode. A tuple of recieved value for each of the 4
+        channels.
+        """
         self.mode = 'IR-REMOTE'
         return self.value0, self.value1, self.value2, self.value3
 
@@ -96,11 +127,22 @@ class InfraredSensor(LegoSensor):
 
     @property
     def prox(self):
+        """Proximity mode. Distance in percent (100% is about 70cm)."""
         self.mode = 'IR-PROX'
         return self.value0
 
     @property
     def seek(self):
+        """IR Seeker mode. A list of (`heading`, `distance`) pairs for each of
+        the 4 channels.
+        
+        When looking in the same direction as the sensor, `heading` =
+        -25 is far left and `heading` = +25 is far right.
+
+        `distance` is the distance in percent (100% is about 70cm).
+
+        Channels with no baecon returns (0, 128).
+        """
         self.mode = 'IR-SEEK'
         return [(self.value0, self.value1),
                 (self.value2, self.value3),
